9a406a9e159d987accbe03acda524808250890ce
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-01-24 11:46:39 -0600

    finished the gemoetry2d stuff (I think)

--- turtlelib/src/geometry2d.cpp
+++ turtlelib/src/geometry2d.cpp
@@ -1,11 +1,56 @@
-#include "turtlelib/geometry2d.hpp" // Replace with the actual path of your header file
+#include "turtlelib/geometry2d.hpp"
+#include <math.h>
+#include <ostream>
+#include <iostream>
 
 namespace turtlelib {
 
     double normalize_angle(double rad) {
-        while (rad > PI) rad -= 2 * PI;
-        while (rad <= -PI) rad += 2 * PI;
-        return rad;
+        return atan2(sin(rad), cos(rad));
+    }
+
+    std::ostream & operator<<(std::ostream & os, const Point2D & p) {
+        os << "[" << p.x << " " << p.y << "]";
+        return os;
+    }
+
+    std::istream& operator>>(std::istream& is, Point2D& p) {
+        char ch;
+        if (is.peek() == '[') {
+            is >> ch >> p.x >> p.y >> ch;
+        } else {
+            is >> p.x >> p.y;
+        }
+        return is;
+    }
+
+    Vector2D operator-(const Point2D & head, const Point2D & tail) {
+        Vector2D vect;
+        vect.x = head.x - tail.x;
+        vect.y = head.y - tail.y;
+        return vect;
+    }
+
+    Point2D operator+(const Point2D & tail, const Vector2D & disp) {
+        Point2D pnt;
+        pnt.x = tail.x - disp.x;
+        pnt.y = tail.y - disp.y;
+        return pnt;
+    }
+
+    std::ostream & operator<<(std::ostream & os, const Vector2D & v) {
+        os << "[" << v.x << " " << v.y << "]";
+        return os;
+    }
+
+    std::istream& operator>>(std::istream & is, Vector2D & v) {
+        char ch;
+        if (is.peek() == '[') {
+            is >> ch >> v.x >> v.y >> ch;
+        } else {
+            is >> v.x >> v.y;
+        }
+        return is;
     }
 
 }
--- turtlelib/tests/test_geometry2d.cpp
+++ turtlelib/tests/test_geometry2d.cpp
@@ -49,19 +49,143 @@ namespace turtlelib {
     }
 
     TEST_CASE("normalize_angle", "geometry2d") {
-        /// Test case conversions found via https://www.rapidtables.com/convert/number/radians-to-degrees.html
-        // 3 * PI rad -> PI rad normalized
-        double t_degree = 3 * PI;
+        // PI rad -> PI rad normalized
+        double t_degree = PI;
         double t_normalized = PI;
         REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
-        // - PI rad -> 0 rad normalized
+        // - PI rad -> PI rad normalized
         t_degree = - PI;
-        t_normalized = PI;
+        t_normalized = - PI;
         REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
-        // 5 * PI/2 rad -> PI/2 rad normalized
-        t_degree = 5 * PI/2;
-        t_normalized = PI/2;
+        // - PI/4 rad -> - PI/4 rad normalized
+        t_degree = - PI/4;
+        t_normalized = - PI/4;
+        REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
+        // 0 rad -> 0 rad normalized
+        t_degree = 0;
+        t_normalized = 0;
+        REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
+        // 3 * PI/2 rad -> - PI/2 rad normalized
+        t_degree = 3 * PI/2;
+        t_normalized = - PI/2;
+        REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
+        // - 5 * PI/2 rad -> PI/2 rad normalized
+        t_degree = - 5 * PI/2;
+        t_normalized = - PI/2;
         REQUIRE_THAT(normalize_angle(t_degree), Catch::Matchers::WithinAbs(t_normalized, TOLERANCE));
     }
 
+    TEST_CASE("output_point", "geometry2d") {
+        // Standard test
+        Point2D t_point{4.1, 3.9};
+        std::stringstream t_os;
+        t_os << t_point;
+        REQUIRE((t_os).str() == "[4.1 3.9]");
+
+        t_os.str("");
+        t_os.clear();
+        // Large values
+        t_point.x = 380.2;
+        t_point.y= 0.00345;
+        t_os << t_point;
+        REQUIRE((t_os).str() == "[380.2 0.00345]");
+
+        t_os.str("");
+        t_os.clear();
+        // Negative values
+        t_point.x = -0.2;
+        t_point.y= -10.8;
+        t_os << t_point;
+        REQUIRE((t_os).str() == "[-0.2 -10.8]");
+    }
+
+    TEST_CASE("input_point", "geometry2d") {
+        Point2D t_point;
+        std::stringstream t_is;
+
+        // Standard test
+        t_is.str("2.9 10.8");
+        t_is >> t_point;
+        REQUIRE(((t_point.x == 2.9) && (t_point.y == 10.8)));
+
+        t_is.str("");
+        t_is.clear();
+        // Input with brackets
+        t_is.str("[304.3 50.6]");
+        t_is >> t_point;
+        REQUIRE(((t_point.x == 304.3) && (t_point.y == 50.6)));
+
+        t_is.str("");
+        t_is.clear();
+        // Negative values
+        t_is.str("-1.2 -3.4056");
+        t_is >> t_point;
+        REQUIRE(((t_point.x == -1.2) && (t_point.y == -3.4056)));
+    }
+
+    TEST_CASE("subtract_points", "geometry2d") {
+        // Standard test
+        REQUIRE_THAT((Point2D{3.4, 0.0} - Point2D{3.4, 5.5}).x, Catch::Matchers::WithinAbs((Vector2D{0.0, -5.5}).x, TOLERANCE));
+        REQUIRE_THAT((Point2D{3.4, 0.0} - Point2D{3.4, 5.5}).y, Catch::Matchers::WithinAbs(Vector2D{0.0, -5.5}.y, TOLERANCE));
+
+        // Standard test no.2
+        REQUIRE_THAT((Point2D{18.4, 9.3} - Point2D{-4.4, 2.1}).y, Catch::Matchers::WithinAbs((Vector2D{22.8, 7.2}).y, TOLERANCE));
+    }
+
+    TEST_CASE("add_point_and_vector", "geometry2d") {
+        // Standard test
+        REQUIRE_THAT((Point2D{3.4, 0.0} + Vector2D{3.4, 5.5}).x, Catch::Matchers::WithinAbs((Point2D{0.0, -5.5}).x, TOLERANCE));
+        REQUIRE_THAT((Point2D{3.4, 0.0} + Vector2D{3.4, 5.5}).y, Catch::Matchers::WithinAbs(Point2D{0.0, -5.5}.y, TOLERANCE));
+
+        // Standard test no.2
+        REQUIRE_THAT((Point2D{18.4, 9.3} + Vector2D{-4.4, 2.1}).y, Catch::Matchers::WithinAbs((Point2D{22.8, 7.2}).y, TOLERANCE));
+    }
+
+    TEST_CASE("output_vector", "geometry2d") {
+        // Standard test
+        Vector2D t_vector{4.1, 3.9};
+        std::stringstream t_os;
+        t_os << t_vector;
+        REQUIRE((t_os).str() == "[4.1 3.9]");
+
+        t_os.str("");
+        t_os.clear();
+        // Large values
+        t_vector.x = 380.2;
+        t_vector.y= 0.00345;
+        t_os << t_vector;
+        REQUIRE((t_os).str() == "[380.2 0.00345]");
+
+        t_os.str("");
+        t_os.clear();
+        // Negative values
+        t_vector.x = -0.2;
+        t_vector.y= -10.8;
+        t_os << t_vector;
+        REQUIRE((t_os).str() == "[-0.2 -10.8]");
+    }
+
+    TEST_CASE("input_vector", "geometry2d") {
+        Vector2D t_vector;
+        std::stringstream t_is;
+
+        // Standard test
+        t_is.str("2.9 10.8");
+        t_is >> t_vector;
+        REQUIRE(((t_vector.x == 2.9) && (t_vector.y == 10.8)));
+
+        t_is.str("");
+        t_is.clear();
+        // Input with brackets
+        t_is.str("[304.3 50.6]");
+        t_is >> t_vector;
+        REQUIRE(((t_vector.x == 304.3) && (t_vector.y == 50.6)));
+
+        t_is.str("");
+        t_is.clear();
+        // Negative values
+        t_is.str("-1.2 -3.4056");
+        t_is >> t_vector;
+        REQUIRE(((t_vector.x == -1.2) && (t_vector.y == -3.4056)));
+    }
 }
