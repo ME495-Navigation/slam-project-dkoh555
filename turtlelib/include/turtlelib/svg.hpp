768a314f1994ae9c1245ffc4ef6474b429562a9b
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-01-25 03:05:40 -0600

    adjusted namespace

--- turtlelib/include/turtlelib/svg.hpp
+++ turtlelib/include/turtlelib/svg.hpp
@@ -8,63 +8,65 @@
 /// \file
 /// \brief SVG image generator
 
-/// \brief SVG image generator class
-class SVG {
+namespace turtlelib
+    {
+    /// \brief SVG image generator class
+    class SVG {
+    private:
+        /// \brief Width of the image
+        int width;
+        /// \brief Height of the image
+        int height;
+        /// \brief Elements of the svg file
+        std::vector<std::string> elements;
 
-private:
-    /// \brief Width of the image
-    int width;
-    /// \brief Height of the image
-    int height;
-    /// \brief Elements of the svg file
-    std::vector<std::string> elements;
+    public:
+        /// \brief Creates the image
+        /// \param w Width of the image
+        /// \param h Height of the image
+        SVG(int w, int h);
 
-public:
-    /// \brief Creates the image
-    /// \param w Width of the image
-    /// \param h Height of the image
-    SVG(int w, int h);
+        /// \brief Saves the image as a named file
+        /// \param filename Desired file name
+        /// \param extra image file that is modified
+        void save_file(const std::string& filename, const std::string& extra);
 
-    /// \brief Saves the image as a named file
-    /// \param filename Desired file name
-    /// \param extra image file that is modified
-    void save_file(const std::string& filename, const std::string& extra);
+        /// \brief Adds a line in the image
+        /// \param x1 x coord of the beginning of the line
+        /// \param x2 x coord of the end of the line
+        /// \param y1 y coord of the beginning of the line
+        /// \param y2 y coord of the end of the line
+        /// \param color Color of the line
+        void draw_line(int x1, int y1, int x2, int y2, const std::string& color);
 
-    /// \brief Adds a line in the image
-    /// \param x1 x coord of the beginning of the line
-    /// \param x2 x coord of the end of the line
-    /// \param y1 y coord of the beginning of the line
-    /// \param y2 y coord of the end of the line
-    /// \param color Color of the line
-    void draw_line(int x1, int y1, int x2, int y2, const std::string& color);
+        /// \brief Adds a rectangle to the image
+        /// \param x Rectangle's x coordinate
+        /// \param y Rectangle's y coordinate
+        /// \param w Rectangle's width
+        /// \param h Rectangle's height
+        void draw_rectangle(int x, int y, int w, int h);
 
-    /// \brief Adds a rectangle to the image
-    /// \param x Rectangle's x coordinate
-    /// \param y Rectangle's y coordinate
-    /// \param w Rectangle's width
-    /// \param h Rectangle's height
-    void draw_rectangle(int x, int y, int w, int h);
+        /// \brief Adds a citcle to the image
+        /// \param x Circle center's x coordinate
+        /// \param y Circle center's y coordinate
+        /// \param rad Circle's radius
+        /// \param color Color of the circle
+        void draw_circle(int x, int y, int rad, const std::string& color);
 
-    /// \brief Adds a citcle to the image
-    /// \param x Circle center's x coordinate
-    /// \param y Circle center's y coordinate
-    /// \param rad Circle's radius
-    /// \param color Color of the circle
-    void draw_circle(int x, int y, int rad, const std::string& color);
-
-    /// \brief Adds text to the image
-    /// \param x Text box's x coordinate
-    /// \param y Text box's y coordinate
-    /// \param text Text added to the image
-    void add_text(int x, int y, const std::string& text);
-    
-    /// \brief Returns the image width
-    /// \return Width of the image
-    int get_width();
-    
-    /// \brief Returns the image height
-    /// \return Height of the image
-    int get_height();
-};
+        /// \brief Adds text to the image
+        /// \param x Text box's x coordinate
+        /// \param y Text box's y coordinate
+        /// \param text Text added to the image
+        void add_text(int x, int y, const std::string& text);
+        
+        /// \brief Returns the image width
+        /// \return Width of the image
+        int get_width();
+        
+        /// \brief Returns the image height
+        /// \return Height of the image
+        int get_height();
+    };
+}
 
 #endif // SVG_HPP
--- turtlelib/src/svg.cpp
+++ turtlelib/src/svg.cpp
@@ -1,47 +1,49 @@
 #include "turtlelib/svg.hpp"
 
-SVG::SVG(int w, int h) : width(w), height(h) {
-    elements.push_back("<svg width=\"" + std::to_string(w) + "\" height=\"" + std::to_string(h) + "\">");
-}
-
-void SVG::save_file(const std::string& filename, const std::string& extra) {
-    elements.push_back(extra);
-    elements.push_back("</svg>");
-    
-    std::ofstream file(filename);
-    if (file.is_open()) {
-        for (const std::string& element : elements) {
-            file << element << "\n";
-        }
-        file.close();
-    } else {
-        std::cerr << "Unable to open file: " << filename << std::endl;
+namespace turtlelib {
+    SVG::SVG(int w, int h) : width(w), height(h) {
+        elements.push_back("<svg width=\"" + std::to_string(w) + "\" height=\"" + std::to_string(h) + "\">");
     }
-}
 
-void SVG::draw_line(int x1, int y1, int x2, int y2, const std::string& color) {
-    elements.push_back("<line x1=\"" + std::to_string(x1) + "\" y1=\"" + std::to_string(y1) +
-                       "\" x2=\"" + std::to_string(x2) + "\" y2=\"" + std::to_string(y2) + "\" stroke=\"" + color + "\" stroke-width=\"2\" marker-start=\"url(#Arrow1Sstart)\"/>");
-}
+    void SVG::save_file(const std::string& filename, const std::string& extra) {
+        elements.push_back(extra);
+        elements.push_back("</svg>");
+        
+        std::ofstream file(filename);
+        if (file.is_open()) {
+            for (const std::string& element : elements) {
+                file << element << "\n";
+            }
+            file.close();
+        } else {
+            std::cerr << "Unable to open file: " << filename << std::endl;
+        }
+    }
 
-void SVG::draw_rectangle(int x, int y, int w, int h) {
-    elements.push_back("<rect x=\"" + std::to_string(x) + "\" y=\"" + std::to_string(y) +
-                       "\" width=\"" + std::to_string(w) + "\" height=\"" + std::to_string(h) + "\"/>");
-}
+    void SVG::draw_line(int x1, int y1, int x2, int y2, const std::string& color) {
+        elements.push_back("<line x1=\"" + std::to_string(x1) + "\" y1=\"" + std::to_string(y1) +
+                        "\" x2=\"" + std::to_string(x2) + "\" y2=\"" + std::to_string(y2) + "\" stroke=\"" + color + "\" stroke-width=\"2\" marker-start=\"url(#Arrow1Sstart)\"/>");
+    }
 
-void SVG::draw_circle(int x, int y, int rad, const std::string& color) {
-    elements.push_back("<circle cx=\"" + std::to_string(x) + "\" cy=\"" + std::to_string(y) +
-                       "\" r=\"" + std::to_string(rad) + "\" stroke=\"" + color + "\" fill=\"" + color + "\" stroke-width=\"1\"/>");
-}
+    void SVG::draw_rectangle(int x, int y, int w, int h) {
+        elements.push_back("<rect x=\"" + std::to_string(x) + "\" y=\"" + std::to_string(y) +
+                        "\" width=\"" + std::to_string(w) + "\" height=\"" + std::to_string(h) + "\"/>");
+    }
 
-void SVG::add_text(int x, int y, const std::string& text) {
-    elements.push_back("<text x=\"" + std::to_string(x) + "\" y=\"" + std::to_string(y) + "\">" + text + "</text>");
-}
+    void SVG::draw_circle(int x, int y, int rad, const std::string& color) {
+        elements.push_back("<circle cx=\"" + std::to_string(x) + "\" cy=\"" + std::to_string(y) +
+                        "\" r=\"" + std::to_string(rad) + "\" stroke=\"" + color + "\" fill=\"" + color + "\" stroke-width=\"1\"/>");
+    }
 
-int SVG::get_width(){
-    return width;
-}
+    void SVG::add_text(int x, int y, const std::string& text) {
+        elements.push_back("<text x=\"" + std::to_string(x) + "\" y=\"" + std::to_string(y) + "\">" + text + "</text>");
+    }
 
-int SVG::get_height(){
-    return height;
+    int SVG::get_width(){
+        return width;
+    }
+
+    int SVG::get_height(){
+        return height;
+    }
 }
