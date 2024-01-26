46da82c219335ff7e24cf639e662466083661f65
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-01-25 22:55:17 -0600

    created new service and implemented the basic teleport service; still have warnings from unused variables

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -12,6 +12,7 @@
 
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "nusim/srv/teleport.hpp"
 
 using std::string;
 
@@ -38,14 +39,23 @@ public:
     auto theta0 = declare_and_get_param<double>("theta0", 0.0f, *this, "Starting directional angle of robot");
 
     //
+    // Additional variable initialization
+    //
+    init_var();
+
+    //
     // PUBLISHERS
     //
+    // Publishing the simulator's timestep count
     timestep_pub = this->create_publisher<std_msgs::msg::UInt64>("timestep", 100);
 
     //
     // SERVICE
     //
+    // Service to reset the simulator
     reset_srv = this->create_service<std_srvs::srv::Empty>("~/reset", std::bind(&nusimNode::reset_callback, this, _1, _2));
+    // Service to teleport the robot
+    teleport_srv = this->create_service<nusim::srv::Teleport>("~/teleport", std::bind(&nusimNode::teleport_callback, this, _1, _2));
 
     //
     // TIMER
@@ -60,6 +70,7 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub;
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv;
+  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_srv;
 
   //
   // Helper functions
@@ -81,17 +92,37 @@ private:
     std_msgs::msg::UInt64 new_msg;
     new_msg.data = timestep;
     RCLCPP_INFO(rclcpp::get_logger("nusim"), "Timestep: %zu", timestep);
+    RCLCPP_INFO_STREAM(get_logger(), "Curr pos [x:" << x << " y:" << y << " theta:" << theta << "]");
     timestep += 1;
     
   }
 
   void reset_callback(std::shared_ptr<std_srvs::srv::Empty::Request>, std::shared_ptr<std_srvs::srv::Empty::Response>)
   {
+    // timestep = 0;
+    // x = x0;
+    // y = y0;
+    // theta = theta0;
+    init_var();
+  }
+
+  void teleport_callback(std::shared_ptr<nusim::srv::Teleport::Request> request, std::shared_ptr<nusim::srv::Teleport::Response> response)
+  {
+    x = request->x;
+    y = request->y;
+    theta = request->theta;
+    RCLCPP_INFO_STREAM(get_logger(), "Teleporting to [x:" << x << " y:" << y << " theta:" << theta << "]");
+    response->success = true;
+  }
+
+  void init_var()
+  {
     timestep = 0;
     x = x0;
     y = y0;
     theta = theta0;
   }
+
 };
 
 int main(int argc, char *argv[])
