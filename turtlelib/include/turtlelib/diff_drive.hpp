c8e77bed13a08b0b0dab7dd768c7819886a6f260
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-02-03 15:40:20 -0600

    setup basic diff_drive foundation

---
+++ turtlelib/include/turtlelib/diff_drive.hpp
@@ -0,0 +1,21 @@
+#ifndef TURTLELIB_DIFF_DRIVE_INCLUDE_GUARD_HPP
+#define TURTLELIB_DIFF_DRIVE_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Two-dimensional rigid body transformations.
+
+
+#include<iosfwd> // contains forward definitions for iostream objects
+
+#include"turtlelib/geometry2d.hpp"
+
+namespace turtlelib
+{
+
+    /// \brief a rigid body transformation in 2 dimensions
+    class DiffDrive
+    {
+
+    };
+}
+
+#endif
