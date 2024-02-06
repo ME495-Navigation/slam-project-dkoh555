f51c38090a60d7b867accc8bfcbd03c972248e04
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-02-05 23:35:41 -0600

    slowly making progress

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -1,10 +1,98 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include <string>
+#include <cmath>
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <map>
 
-int main(int argc, char ** argv)
+#include "geometry_msgs/msg/twist.hpp"
+
+using std::string;
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+class TurtleControlNode : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+  public:
+    //
+    // CONSTRUCTOR
+    //
+    TurtleControlNode() : Node("turtle_control")
+    {
+      //
+      // PARAMETERS
+      //
+      auto param_desc = rcl_interfaces::msg::ParameterDescriptor{}; // Prepare for parameter descriptions
 
-  printf("hello world nuturtle_control package\n");
+      param_desc.description = "Frequency of node timer";
+      declare_parameter("frequency", 95.0, param_desc);
+      frequency = get_parameter("frequency").as_double();
+
+      //
+      // Additional variable initialization
+      //
+      init_var();
+
+      //
+      // PUBLISHERS
+      //
+      cmd_vel_pub = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 100);
+
+      //
+      // SERVICE
+      //
+
+      //
+      // BROADCASTER
+      //
+
+      //
+      // TIMER
+      //
+      timer = create_wall_timer(1.0s / frequency, std::bind(&TurtleControlNode::timer_callback, this));
+    }
+
+  private:
+    //
+    // Node-related Declarations
+    //
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
+
+    //
+    // Variables
+    //
+    double frequency;
+
+    //
+    // TIMER CALLBACK
+    //
+    /// @brief Timer callback for node, reads joy_state to publish appropriate output messages
+    void timer_callback()
+    {
+
+    }
+
+    //
+    // HELPER FUNCTIONS
+    //
+    void init_var()
+    {
+
+    }
+
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TurtleControlNode>());
+  rclcpp::shutdown();
   return 0;
 }
