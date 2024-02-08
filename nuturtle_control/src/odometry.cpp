efa8ae4a921741ad7253122d198e6281abec27e6
dkoh555 <damienkoh2025@u.northwestern.edu>
2024-02-07 20:31:26 -0600

    reformated code

--- nuturtle_control/src/odometry.cpp
+++ nuturtle_control/src/odometry.cpp
@@ -30,74 +30,73 @@ using std::placeholders::_2;
 
 class OdometryNode : public rclcpp::Node
 {
-  public:
+public:
     //
     // CONSTRUCTOR
     //
     OdometryNode() : Node("odometry")
     {
-      //
-      // PARAMETERS
-      //
-      auto param_desc = rcl_interfaces::msg::ParameterDescriptor{}; // Prepare for parameter descriptions
+        //
+        // PARAMETERS
+        //
+        auto param_desc = rcl_interfaces::msg::ParameterDescriptor{}; // Prepare for parameter descriptions
 
-      param_desc.description = "Frequency of node timer";
-      declare_parameter("frequency", 95.0, param_desc);
-      frequency = get_parameter("frequency").as_double();
+        param_desc.description = "Frequency of node timer";
+        declare_parameter("frequency", 95.0, param_desc);
+        frequency = get_parameter("frequency").as_double();
 
-      param_desc.description = "Frame ID of body frame";
-      declare_parameter("body_id", "UNUSED", param_desc);
-      body_id = get_parameter("body_id").as_string();
+        param_desc.description = "Frame ID of body frame";
+        declare_parameter("body_id", "UNUSED", param_desc);
+        body_id = get_parameter("body_id").as_string();
 
-      param_desc.description = "Frame ID of odom frame";
-      declare_parameter("odom_id", "odom", param_desc);
-      odom_id = get_parameter("odom_id").as_string();
+        param_desc.description = "Frame ID of odom frame";
+        declare_parameter("odom_id", "odom", param_desc);
+        odom_id = get_parameter("odom_id").as_string();
 
-      param_desc.description = "Frame ID of left wheel";
-      declare_parameter("wheel_left", "UNUSED", param_desc);
-      wheel_left = get_parameter("wheel_left").as_string();
+        param_desc.description = "Frame ID of left wheel";
+        declare_parameter("wheel_left", "UNUSED", param_desc);
+        wheel_left = get_parameter("wheel_left").as_string();
 
-      param_desc.description = "Frame ID of right wheel";
-      declare_parameter("wheel_right", "UNUSED", param_desc);
-      wheel_right = get_parameter("wheel_right").as_string();
+        param_desc.description = "Frame ID of right wheel";
+        declare_parameter("wheel_right", "UNUSED", param_desc);
+        wheel_right = get_parameter("wheel_right").as_string();
 
-      if(check_params_string())
-      {
-        RCLCPP_ERROR(this->get_logger(), "Required paramters not provided");
-        rclcpp::shutdown();
-      }
-      
+        if (check_params_string())
+        {
+            RCLCPP_ERROR(this->get_logger(), "Required paramters not provided");
+            rclcpp::shutdown();
+        }
 
-      //
-      // Additional variable initialization
-      //
-      init_var();
+        //
+        // Additional variable initialization
+        //
+        init_var();
 
-      //
-      // SUBSCRIBERS
-      //
-      joint_states_sub = create_subscription<sensor_msgs::msg::JointState>("joint_states", 10, std::bind(&OdometryNode::joint_states_callback, this, _1));
+        //
+        // SUBSCRIBERS
+        //
+        joint_states_sub = create_subscription<sensor_msgs::msg::JointState>("joint_states", 10, std::bind(&OdometryNode::joint_states_callback, this, _1));
 
-      //
-      // PUBLISHERS
-      //
-      odom_pub = create_publisher<nav_msgs::msg::Odometry>("odom", 100);
+        //
+        // PUBLISHERS
+        //
+        odom_pub = create_publisher<nav_msgs::msg::Odometry>("odom", 100);
 
-      //
-      // SERVICE
-      //
+        //
+        // SERVICE
+        //
 
-      //
-      // BROADCASTER
-      //
+        //
+        // BROADCASTER
+        //
 
-      //
-      // TIMER
-      //
-      timer = create_wall_timer(1.0s / frequency, std::bind(&OdometryNode::timer_callback, this));
+        //
+        // TIMER
+        //
+        timer = create_wall_timer(1.0s / frequency, std::bind(&OdometryNode::timer_callback, this));
     }
 
-  private:
+private:
     //
     // Node-related Declarations
     //
@@ -109,7 +108,7 @@ class OdometryNode : public rclcpp::Node
     // Variables
     //
     double frequency;
-    std::string body_id, odom_id, wheel_left, wheel_right; 
+    std::string body_id, odom_id, wheel_left, wheel_right;
 
     //
     // Objects
@@ -123,7 +122,6 @@ class OdometryNode : public rclcpp::Node
     /// @brief Timer callback for node, reads joy_state to publish appropriate output messages
     void timer_callback()
     {
-
     }
 
     //
@@ -140,8 +138,8 @@ class OdometryNode : public rclcpp::Node
 
         // Note the change in WheelPosition
         WheelPosition delta_wheel_position{curr_wheel_position.right - old_wheel_position.right,
-                                            curr_wheel_position.left - old_wheel_position.left};
-        
+                                           curr_wheel_position.left - old_wheel_position.left};
+
         // Update the robot configuration with forward_k
         turtlebot.forward_k(delta_wheel_position);
 
@@ -165,7 +163,7 @@ class OdometryNode : public rclcpp::Node
         geometry_msgs::msg::Quaternion robot_quaternion;
         tf2::convert(tf2_robot_quaternion, robot_quaternion);
         robot_pose.orientation = robot_quaternion;
-        
+
         // Retrieve the current twist of the robot and fill in twist message information
         Twist2D curr_twist = turtlebot.get_twist();
         geometry_msgs::msg::Twist robot_twist;
@@ -182,11 +180,11 @@ class OdometryNode : public rclcpp::Node
     //
     // HELPER FUNCTIONS
     //
-    /// @brief 
+    /// @brief
     void init_var()
     {
-      // Initialize the diff drive robot
-      turtlebot = DiffDrive();
+        // Initialize the diff drive robot
+        turtlebot = DiffDrive();
     }
 
     bool check_params_string()
@@ -197,8 +195,8 @@ class OdometryNode : public rclcpp::Node
 
 int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<OdometryNode>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<OdometryNode>());
+    rclcpp::shutdown();
+    return 0;
 }
